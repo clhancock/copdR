6e195b5ae7494d8a241fe4d816f119c91f3e9fbd
Aubrey LaPlante <143374886+AubreyLaPlante@users.noreply.github.com>
2024-01-31 09:38:00 -0700

    edits to bs_import

---
+++ R/bs_import.R
@@ -0,0 +1,89 @@
+#' Import and organize backslip file names into a character vector
+#'
+#' @return A character vector listing all the names of .mat files
+#' @export
+#'
+
+
+list.mat <- function(){
+     #generate a vector of all backslip data names
+     path <- as.character(readline(prompt = "Enter backslip folder name:   "))
+     bs.names <- list.files(path = paste0(getwd(),"/",path), pattern = ".mat")
+
+     if(length(list.files(path = paste0(getwd(),"/",path), pattern = ".mat")) == 0){
+          stop("ERROR: Folder not found. Check that the folder is in your working drive, and that the folder name is spelled correctly.")
+     }else{
+          return(bs.names)
+     }
+
+}
+
+
+
+
+#' Combine .mat file data
+#'
+#'
+#' Combine all offset data from the mat files
+#' Make sure each file name starts with a maximum 4-letter/number identifier
+#'
+#' @param folder_name A folder in your working directory that contains all
+#'   the .mat files. Name should be in quotations, i.e. "folder_name"
+#' @param bs_names A vector with the names of the backslip files
+#'
+#' @return A list of three matrices including lateral, vertical and total offset.
+#' @export
+#'
+#'
+#'
+
+matrix.mat <- function(folder_name, bs_names){
+     bs.init <- R.matlab::readMat(file.path(folder_name, bs_names[1]))
+     #initialize a matrix from the first .mat data
+     h_x = t(stats::na.omit(bs.init$XCORDATAH))
+     h_y = t(stats::na.omit(bs.init$XCOR.SUMH))
+     lateral = dplyr::bind_cols(h_x, h_y)
+     colnames(lateral) = suppressMessages(c("h_x","h_y"))
+
+     z_x = t(stats::na.omit(bs.init$XCORDATAV))
+     z_y = t(stats::na.omit(bs.init$XCOR.SUMV))
+     vertical = dplyr::bind_cols(x = z_x, y = z_y)
+     colnames(vertical) = suppressMessages(c("z_x","z_y"))
+
+     t_x = t(stats::na.omit(bs.init$XCORDATAT))
+     t_y = t(stats::na.omit(bs.init$XCOR.SUMT))
+     total = dplyr::bind_cols(x = t_x, y = t_y)
+     colnames(total) = suppressMessages(c("t_x","t_y"))
+
+     suppressMessages(for(i in 2:length(bs_names)){
+
+          pathname <- file.path(folder_name, bs_names[i])
+          bs.data <- R.matlab::readMat(pathname)
+
+          h_x = t(stats::na.omit(bs.data$XCORDATAH))
+          h_y = t(stats::na.omit(bs.data$XCOR.SUMH))
+          h_all = dplyr::bind_cols(h_x,h_y)
+          colnames(h_all) = c("h_x","h_y")
+
+          z_x = t(stats::na.omit(bs.data$XCORDATAV))[1]
+          z_y = t(stats::na.omit(bs.data$XCOR.SUMV))[2]
+          z_all = dplyr::bind_cols(z_x,z_y)
+          colnames(z_all) = c("z_x","z_y")
+
+          t_x = t(stats::na.omit(bs.data$XCORDATAT))
+          t_y = t(stats::na.omit(bs.data$XCOR.SUMT))
+          t_all = dplyr::bind_cols(t_x, t_y)
+          colnames(t_all) = c("t_x","t_y")
+
+          lateral <- dplyr::left_join(lateral,h_all, by = "h_x")
+          vertical <- dplyr::left_join(vertical,z_all, by = "z_x")
+          total <- dplyr::left_join(total,t_all, by = "t_x")
+     })
+     #renaming
+     colnames(lateral)[2:(length(bs_names)+1)] <- substr(bs_names,1,4)
+     colnames(vertical)[2:(length(bs_names)+1)] <- substr(bs_names,1,4)
+     colnames(total)[2:(length(bs_names)+1)] <- substr(bs_names,1,4)
+     return(list(backslip_lat = lateral,
+                 backslip_vert = vertical,
+                 backslip_tot = total))
+}
